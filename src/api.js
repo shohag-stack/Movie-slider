cbf259a15fce700fd6109bb759239a0b30954068
shohag-stack <shohag4y@gmail.com>
2024-07-13 03:14:21 +0600

    env file updated

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-const API_KEY = process.env.NEXT_PUBLIC_API_KEY; // Replace with your actual API key
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const API_KEY = import.meta.env.VITE_API_KEY; // Replace with your actual API key
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const fetchTrendingMovies = async () => {
     const response = await fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`);
